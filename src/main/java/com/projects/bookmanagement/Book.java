fd43394d54642271c8c4ea8b48533159621c0b4b
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-05-06 18:27:42 +0200

    Added Book class that represents the basic object in our application

---
+++ src/main/java/com/projects/bookmanagement/Book.java
@@ -0,0 +1,12 @@
+package com.projects.bookmanagement;
+
+public class Book {
+    //to do add variables that the book could have
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private int price;
+    private int stock;
+    private int numberOfPages;
+}
