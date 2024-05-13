300300b50793a3a427fa95a091b64662da2d4ef7
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-05-13 18:36:00 +0200

    added getters and setters to Book and changed id's in main-view

--- src/main/java/com/projects/bookmanagement/Book.java
+++ src/main/java/com/projects/bookmanagement/Book.java
@@ -1,5 +1,12 @@
 package com.projects.bookmanagement;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Book {
     //to do add variables that the book could have
     private int id;
@@ -9,4 +16,71 @@ public class Book {
     private int price;
     private int stock;
     private int numberOfPages;
+
+
+    public static List<Book> readFromJson(String path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(path), objectMapper.getTypeFactory().constructCollectionType(List.class, Book.class));
+    }
+
+    public void writeToJson() {
+        File file = new File("assets/books.json");
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
 }
