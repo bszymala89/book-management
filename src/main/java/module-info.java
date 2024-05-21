5389bddb087d9e5a26e3da32edeb8d012fe0746c
bszymala89 <72854357+bszymala89@users.noreply.github.com>
2024-05-21 18:24:52 +0200

    added new dependency for objectMapper

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.projects.bookmanagement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.projects.bookmanagement to javafx.fxml;
